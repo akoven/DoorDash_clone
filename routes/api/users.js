2c0b7c1aaac13807eff811a759e3294f785b0daf
Attiya <adarensburg@live.com>
2023-02-12 21:44:31 -0600

    validations added for user registration and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,8 @@ const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // router.get('/test', (req,res) => res.json({msg:"this is the users route"}));
 router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
@@ -15,11 +17,18 @@ router.get('/current', passport.authenticate('jwt', {session: false}), (req, res
 });
 
 router.post('/register',(req,res) =>{
+    const {errors, isValid} = validateRegisterInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors);
+    };
 
     User.findOne({email: req.body.email}).then(user =>{
 
         if(user){
-            return res.status(400).json({email:"A user has already registered with this email address"})
+            errors.email = 'Email already exists!';
+            // return res.status(400).json({email:"A user has already registered with this email address"})
+            return res.status(400).json(errors);
         }else{
             const newUser = new User({
                 handle: req.body.handle,
@@ -44,23 +53,34 @@ router.post('/register',(req,res) =>{
 
 router.post('/login',(req,res) =>{
 
+    const {errors, isValid} = validateLoginInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors);
+    };
+
     const email = req.body.email;
     const password = req.body.password;
 
     User.findOne({email}).then(user => {
         if(!user){
-            return res.status(404).json({email: 'this user does not exist'})
+            errors.email = 'User not found';
+            return res.status(404).json(errors);
+            // return res.status(404).json({email: 'this user does not exist'})
         }
         bcrypt.compare(password, user.password).then(isMatch => {
             if(isMatch){
-                const payload = {id:user.id,handle:user.handle};
-                jwt.sign(payload,keys.secretOrKey,{expiresIn: 7776000},(err,token) =>{
-                    res.json({
-                        success: true,
-                        token: 'Bearer '+token
-                    });
-                })}else{
-                    return res.status(400).json({password: 'Incorrect password'})
+                res.json({msg: 'Success!'});
+
+                // const payload = {id:user.id,handle:user.handle};
+                // jwt.sign(payload,keys.secretOrKey,{expiresIn: 7776000},(err,token) =>{
+                //     res.json({
+                //         success: true,
+                //         token: 'Bearer '+token
+                //     });
+            }else{
+                errors.password = 'Incorrect password';
+                return res.status(400).json(errors);
             };
         });
     });
