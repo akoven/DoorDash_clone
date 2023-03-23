74328ed0ea7e05eefc69b78ab5323b04db3e1922
Attiya <adarensburg@live.com>
2023-03-23 13:08:27 -0500

    added session forms and new reducers

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import session from './session_reducer';
+import SessionErrorsReducer from './session_errors_reducer';
 
 const RootReducer = combineReducers({
-    session
+    session,
+    SessionErrorsReducer
 });
 
 export default RootReducer;
